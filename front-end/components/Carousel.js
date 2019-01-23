73ffe5ebb3e2dadaacd5d176e1961b2569c3d34c
joelmu <joel.muittari@gmail.com>
2019-01-23 23:35:50 +0200

    sub screen working. Ome fixes needed

--- front-end/components/Carousel.js
+++ front-end/components/Carousel.js
@@ -1,14 +1,16 @@
 import React from "react";
 import {
+  Animated,
   StyleSheet,
   View,
   Text,
   Dimensions,
-  Image
+  Image,
+  Platform
 } from "react-native";
 
 const { width: screenWidth } = Dimensions.get('window');
-const width = screenWidth - 125;
+const width = screenWidth -100;
 
 export default class SweetCarousel extends React.Component {
   static WIDTH = width;
@@ -17,14 +19,151 @@ export default class SweetCarousel extends React.Component {
     const { animatedValue, box, index } = this.props;
 
     return (
-      <View>
+      <Animated.View 
+          style={[ styles.container,
+          { transform: [
+        {
+          scale: animatedValue.interpolate({
+            inputRange: [index - 1, index, index + 1],
+            outputRange: [.6, 1.0, .6],
+            extrapolate: 'clamp',
+          }),
+        }
+      ],
+    }]}>
         <View>
           <Image
+            style={ styles.image}
             source={box.image}
           />
+          <View style={styles.priceTag}>
+          <Text style={styles.priceText}>{box.price}</Text>
+          </View>
         </View>
-        <Text>{box.title}</Text>
-      </View>
+        <Animated.View style={styles.textContainer}>
+          <View style={styles.textTag}>
+            <Text style={styles.textTagText}>{box.tag}</Text>
+          </View>
+          <Text style={styles.title}>{box.title}</Text>
+          <View style={styles.valueContainer}>
+            <Text style={styles.valueText}>{box.size}</Text>
+            <View style={styles.circle}></View>
+            <Text style={styles.valueText}>{box.divprice}</Text>
+          </View>
+          <Text style={styles.description}>{box.description}</Text>
+        </Animated.View>
+      </Animated.View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: width,
+    height: width*1.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'visible'
+  },
+	image: Platform.select({
+    ios: {
+      width: width - 95,
+      height: width - 75,
+      overflow: 'visible',
+    },
+    android: {
+      width: width - 120,
+      height: width -100,
+      overflow: 'visible',
+    },
+  }),
+  priceTag: {
+    position: 'absolute',
+    width: 75,
+    height: 75,
+    left: 217,
+    top: -30,
+		borderRadius: 38,
+    backgroundColor: '#AA3C3B',
+    shadowRadius: 4,
+    shadowOffset: {height: 0, width: 4},
+    shadowColor: 'rgba(91, 91, 91, 0.25)',
+    elevation: 4
+  },
+  priceText: {
+    paddingTop: 22,
+    paddingLeft: 10,
+
+    //fontFamily: 'Raleway',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 22,
+
+    color: '#FFFFFF',
+  },
+  textTag: {
+    width: 127,
+    height: 27,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#AA3C3B',
+    shadowRadius: 4,
+    shadowOffset: {height: 0, width: 4},
+    shadowColor: 'rgba(91, 91, 91, 0.25)',
+    elevation: 4,
+    borderRadius: 13.5,
+    marginBottom: 17
+  },
+  textTagText: {
+    //fontFamily: 'Raleway',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 16,
+    textAlign: 'center',
+
+    color: '#FFFFFF',
+  },
+  textContainer: {
+    paddingTop: 18,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    //fontFamily: 'Raleway',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 30,
+    textAlign: 'center',
+    color: '#282828'
+  },
+  valueContainer: {
+    flexDirection: 'row',
+    paddingTop: 11,
+    paddingBottom: 17
+  },
+  circle: {
+    marginTop: 6,
+    marginLeft: 12,
+    marginRight: 12,
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: '#AA3C3B'
+  },
+  valueText: {
+    //fontFamily: 'Raleway',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 14,
+    textAlign: 'center',
+    color: '#979797'
+  },
+  description: {
+    //fontFamily: 'Raleway',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: 14,
+    textAlign: 'center',
+    color: '#282828'
+  }
+});
--- front-end/screens/SubscriptionSelectionScreen.js
+++ front-end/screens/SubscriptionSelectionScreen.js
@@ -1,16 +1,19 @@
 import React from "react";
 import {
 	StyleSheet,
-	View
+  View,
+  Dimensions,
+  Image,
+  Text
 } from "react-native";
-import { Dimensions } from 'react-native';
+import { Constants } from 'expo';
 import SideSwipe from 'react-native-sideswipe';
 
 import Carousel from "../components/Carousel.js"
-
-const boxes = [
-  { title: 'Mixed Box', image: require('../assets/images/mixed.png') },
-  { title: 'Vegan Box', image: require('../assets/images/vegan.png') }
+const boxes = [  
+  { title: 'Mixed Box', price: '199 €', size: '5 kg/box', divprice: '3-4€/meal', description: 'Best of both worl. Can include beef, pork , chicken, milk, eggs, potatoes, brocoli, cabbage... Check to see more details.', tag: 'Best seller' , image: require('../assets/images/mixed.png') },
+  { title: 'Vegan Box', price: '299 €', size: '5 kg/box', divprice: '3-4€/meal', description: 'Best of both worl. Can include beef, pork , chicken, milk, eggs, potatoes, brocoli, cabbage... Check to see more details.', tag: 'Vegan', image: require('../assets/images/vegan.png') },
+  { title: 'Meat Box', price: '499 €',  size: '5 kg/box', divprice: '3-4€/meal', description: 'Best of both worl. Can include beef, pork , chicken, milk, eggs, potatoes, brocoli, cabbage... Check to see more details.', tag: 'Meat', image: require('../assets/images/meat.png') }
 ];
 
 export default class SubscriptionSelectionScreen extends React.Component {
@@ -24,30 +27,62 @@ export default class SubscriptionSelectionScreen extends React.Component {
 
 	render() {
 		const { width } = Dimensions.get('window');
-    const contentOffset = (width - Carousel.WIDTH) / 2;
+    const contentOffset = (width - Carousel.WIDTH) / 2 ;
 
 		return (
-			<View>
-				<View style={styles.circle}>
-				</View>
-				<SideSwipe
-        index={this.state.currentIndex}
-        itemWidth={Carousel.WIDTH}
-        style={{ width }}
-        data={boxes}
-        contentOffset={contentOffset}
-        onIndexChange={index =>
-          this.setState(() => ({ currentIndex: index }))
-        }
-        renderItem={({ itemIndex, currentIndex, item, animatedValue }) => (
-         <Carousel
-            box= {item}
-            index={itemIndex}
-            currentIndex={currentIndex}
-            animatedValue={animatedValue}
+			<View style={{flex: 1}}>
+				<View style={{flex: 1}}>
+          <View style={styles.circle}></View>
+          <View style={styles.topBar}>
+            <Image 
+              style={{width: 16, height: 16}}
+              source={require('../assets/images/previous.png')}
+              />
+            <Text style={styles.topText}>Choose your subscription</Text>
+            <Image 
+              style={{width: 18, height: 18}}
+              source={require('../assets/images/cart.png')}
+              />
+          </View>
+        </View>
+				<View style={{flex: 9}}>
+          <SideSwipe
+            index={this.state.currentIndex}
+            itemWidth={Carousel.WIDTH}
+            threshold={Carousel.WIDTH / 6}
+            style={{ width }}
+            contentContainerStyle={{  paddingTop: 20 }}
+            data={boxes}
+            contentOffset={contentOffset}
+            onIndexChang1e={index =>
+              this.setState(() => ({ currentIndex: index }))
+            }
+            renderItem={({ itemIndex, currentIndex, item, animatedValue }) => (
+             <Carousel
+                box= {item}
+                index={itemIndex}
+                currentIndex={currentIndex}
+                animatedValue={animatedValue}
+              />
+            )}
           />
-        )}
-      />
+        </View>
+        <View style={{flex: 1, justifyContent: 'center'}}>
+          <View style={{alignItems: 'center'}}>
+            <View style={styles.plusCircle}>
+              <Image
+                style={styles.plus}
+                source={require('../assets/images/plus.png')}
+              />
+            </View>
+          </View>
+          <Text style={styles.total}>TOTAL: 549€</Text>
+        </View>
+        <View style={[{flex: 1}, styles.bottom]}>
+          <View style={styles.orderButton}>
+            <Text style={styles.orderText}>Confirm order</Text>
+          </View>
+        </View>
 			</View>
 		);
 	}
@@ -61,5 +96,64 @@ const styles = StyleSheet.create({
 		top: -321,
 		borderRadius: 311,
 		backgroundColor: "#FCB79A"
-	}
+  },
+  topBar: {
+    flexDirection: 'row',
+    paddingTop: 40,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 30
+  },
+  topText: {
+    //fontFamily: 'Raleway',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#AA3C3B'
+  },
+  plusCircle: {
+    //position: absolute;
+    width: 48,
+    height: 48,
+    //left: 183px;
+    //top: 576px;
+    borderWidth: 2,
+    borderColor: '#AA3C3B',
+    borderRadius: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: -25
+  },
+  plus: {
+    width: 23.17, 
+    height: 23.17,
+  },
+  total: {
+    //fontFamily: 'Raleway',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 14,
+    textAlign: 'center',
+    color: '#AA3C3B',
+    paddingTop: 16
+  },
+  orderButton: {
+    width: this.width,
+    height: 56,
+    backgroundColor: '#AA3C3B',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  orderText: {
+    //fontFamily: 'Raleway',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#FFFFFF'
+  },
+  bottom: {
+    justifyContent: 'flex-end'
+  }
 });
