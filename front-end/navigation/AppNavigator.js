07e333c3b99260107d665488f5ea650b3cb98966
JoeNg93 <ntuandung93@gmail.com>
2019-01-29 00:10:56 +0200

    Connect Onboarding and Auth Screens

--- front-end/navigation/AppNavigator.js
+++ front-end/navigation/AppNavigator.js
@@ -1,12 +1,18 @@
-import React from "react";
-import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
-import MainTabNavigator from "./MainTabNavigator";
+import MainTabNavigator from './MainTabNavigator';
+import AuthStack from '../screens/AuthScreens/AuthStack';
+import OnboardingScreen from '../screens/OnboardingScreens/OnboardingScreen';
 
 export default createAppContainer(
-  createSwitchNavigator({
-    // You could add another route here for authentication.
-    // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-    Main: MainTabNavigator
-  })
+  createSwitchNavigator(
+    {
+      // You could add another route here for authentication.
+      // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+      Onboarding: OnboardingScreen,
+      Auth: AuthStack,
+      Main: MainTabNavigator,
+    },
+    { initialRouteName: 'Onboarding' }
+  )
 );
