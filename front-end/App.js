5ea8d2578be9da4d2e17863bc6a9487dce138793
JoeNg93 <ntuandung93@gmail.com>
2019-02-03 17:27:09 +0200

    Add Raleway font

--- front-end/App.js
+++ front-end/App.js
@@ -49,6 +49,7 @@ export default class App extends React.Component {
         // We include SpaceMono because we use it in HomeScreen.js. Feel free
         // to remove this if you are not using it in your app
         'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+        Raleway: require('./assets/fonts/Raleway-Medium.ttf'),
       }),
     ]);
   };
