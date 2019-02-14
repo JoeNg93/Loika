5913ee6c1125ef6b76ee6e47c165ba22a5a9f01b
Dmitrii Stetcko <dimastetsko@gmail.com>
2019-02-14 04:36:16 +0200

    Order history screen and component

---
+++ front-end/components/OrderHistoryBox.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import { StyleSheet, View, Image, Text, TouchableOpacity } from 'react-native';
+import { Icon } from 'react-native-elements';
+import Colors from '../constants/Colors';
+import commonStyles from '../constants/commonStyles';
+import PropTypes from 'prop-types';
+
+
+export default function SubscriptionBox({ orderID, orderDate, orderPrice, isActive }) {
+  return (
+    <View
+      style={[
+        styles.boxContainer,
+        isActive ? styles.boxActive : styles.boxInactive,
+      ]}
+    >
+      <Image
+        source={require('../assets/images/slide4.png')}
+        style={styles.image}
+			/>
+			
+			<View style={styles.mainTextContainer}>
+				<Text style={styles.orderDate}>Date: {orderDate}</Text>
+				<Text style={styles.orderID}>Order ID: {orderID}</Text>
+			</View>
+			
+			<View style={styles.divider} />
+
+			<Text style={styles.orderPrice}>{orderPrice} €</Text>
+
+			<TouchableOpacity
+				style={styles.arrowIcon}
+			>
+				<Icon
+					name={'arrow-forward'}
+					size={21}
+					color={Colors.macaroniCheese}
+				/>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+SubscriptionBox.propTypes = {
+	orderDate: PropTypes.string,
+	orderID: PropTypes.number,
+	orderPrice: PropTypes.string,
+  isActive: PropTypes.bool,
+};
+
+SubscriptionBox.defaultProps = {
+	orderDate: '',
+	orderPrice: '',
+  isActive: true,
+};
+
+const styles = StyleSheet.create({
+	boxContainer: {
+		marginBottom: 21,
+		width: 347,
+		height: 87,
+		borderRadius: 11,
+
+		backgroundColor: Colors.white,
+		borderBottomWidth: 4,
+
+		shadowColor: Colors.darkGrey,
+		shadowOffset: { width: 0, height: 4 },
+		shadowOpacity: 0.25,
+		shadowRadius: 4,
+
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	boxActive: {
+    borderColor: Colors.macaroniCheese,
+  },
+  boxInactive: {
+    borderColor: Colors.lightGrey,
+	},
+	image: {
+		height: 53,
+		width: 71, 
+		marginLeft: 14,
+		marginBottom: 18,
+		marginTop: 16,
+	},
+	mainTextContainer: {
+		marginBottom: 26,
+		marginTop: 26,
+		marginLeft: 21,
+	},
+	orderID: {
+		...commonStyles.fontRalewayRegular,
+		...commonStyles.textGrey,
+		fontSize: 13,
+		marginTop: 5,
+	},
+	orderDate: {
+    ...commonStyles.fontRalewayBold,
+    ...commonStyles.textBlack,
+    fontSize: 14,
+    textTransform: 'uppercase',
+  },
+	divider: {
+		borderColor: '#E1E1E1',
+		height: 25,
+		borderWidth: 0.75,
+		marginRight: 18,
+		marginLeft: 12,
+		marginTop: 31,
+		marginBottom: 30,
+	},
+	orderPrice: {
+		...commonStyles.fontMontserratBold,
+		...commonStyles.textMediumCarmine,
+		fontSize: 16,
+		marginTop: 35,
+		marginBottom: 34,
+	},
+	arrowIcon: {
+		marginLeft: 18,
+		marginRight: 21,
+	},
+});
