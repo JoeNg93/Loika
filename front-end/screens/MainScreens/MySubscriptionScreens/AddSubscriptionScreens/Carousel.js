b131ad9e1d328a25862cae5dcd81b7f4b9f8bc45
JoeNg93 <ntuandung93@gmail.com>
2019-02-20 00:05:29 +0200

    Reformat code

--- front-end/screens/MainScreens/MySubscriptionScreens/AddSubscriptionScreens/Carousel.js
+++ front-end/screens/MainScreens/MySubscriptionScreens/AddSubscriptionScreens/Carousel.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 import {
   Animated,
   StyleSheet,
   View,
   Dimensions,
   Image,
-  Platform
-} from "react-native";
+  Platform,
+} from 'react-native';
 
 const { width: screenWidth } = Dimensions.get('window');
 const width = 200;
@@ -18,22 +18,26 @@ export default class SweetCarousel extends React.Component {
     const { animatedValue, box, index } = this.props;
 
     return (
-      <Animated.View 
-          style={[ styles.container,
-          { transform: [
+      <Animated.View
+        style={[
+          styles.container,
           {
-            scale: animatedValue.interpolate({
-              inputRange: [index - 1, index, index + 1],
-              outputRange: [.6, 1.0, .6],
-              extrapolate: 'clamp',
-            }),
-          }
-      ],
-    }]}>
+            transform: [
+              {
+                scale: animatedValue.interpolate({
+                  inputRange: [index - 1, index, index + 1],
+                  outputRange: [0.6, 1.0, 0.6],
+                  extrapolate: 'clamp',
+                }),
+              },
+            ],
+          },
+        ]}
+      >
         <View>
           <Image
-            style={ styles.image}
-            source={box.image}
+            style={styles.image}
+            source={require('../../../../assets/images/meat.png')}
           />
         </View>
       </Animated.View>
@@ -49,18 +53,18 @@ const styles = StyleSheet.create({
       justifyContent: 'center',
       alignItems: 'center',
       overflow: 'visible',
-      marginTop: 75
+      marginTop: 75,
     },
     android: {
       width: width,
-      height: width*1.2,
+      height: width * 1.2,
       justifyContent: 'center',
       alignItems: 'center',
       overflow: 'visible',
-      marginTop: 80
-    }
+      marginTop: 80,
+    },
   }),
-	image: Platform.select({
+  image: Platform.select({
     ios: {
       width: width,
       height: width,
@@ -72,5 +76,4 @@ const styles = StyleSheet.create({
       overflow: 'visible',
     },
   }),
-  
 });
