60d96b6ba3fd20bae72135a339c5209db83d71d7
JoeNg93 <ntuandung93@gmail.com>
2019-02-21 03:01:36 +0200

    Fix authorization key

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -9,7 +9,7 @@ const server = createServer();
 // Use express middlware to populate current user
 // Decode JWT to get user id per each request
 server.express.use((req, res, next) => {
-  const token = req.headers.Authorization;
+  const token = req.headers.authorization;
   if (token) {
     const { userId } = jwt.verify(token, process.env.APP_SECRET);
     // put the userId onto the req for future requests to access
