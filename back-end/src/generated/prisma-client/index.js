58bb0c252f05dd3da83fe3ac943491c83c46c2d3
JoeNg93 <ntuandung93@gmail.com>
2019-02-24 00:32:25 +0200

    Add API endpoint secret

--- back-end/src/generated/prisma-client/index.js
+++ back-end/src/generated/prisma-client/index.js
@@ -1,41 +1,42 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-var prisma_lib_1 = require('prisma-client-lib');
-var typeDefs = require('./prisma-schema').typeDefs;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
 
 var models = [
   {
-    name: 'Address',
-    embedded: false,
+    name: "Address",
+    embedded: false
   },
   {
-    name: 'CartItem',
-    embedded: false,
+    name: "CartItem",
+    embedded: false
   },
   {
-    name: 'Order',
-    embedded: false,
+    name: "Order",
+    embedded: false
   },
   {
-    name: 'OrderItem',
-    embedded: false,
+    name: "OrderItem",
+    embedded: false
   },
   {
-    name: 'Permission',
-    embedded: false,
+    name: "Permission",
+    embedded: false
   },
   {
-    name: 'Subscription',
-    embedded: false,
+    name: "Subscription",
+    embedded: false
   },
   {
-    name: 'User',
-    embedded: false,
-  },
+    name: "User",
+    embedded: false
+  }
 ];
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: process.env.PRISMA_ENDPOINT,
+  endpoint: `${process.env["PRISMA_ENDPOINT"]}`,
+  secret: `${process.env["PRISMA_SECRET"]}`
 });
 exports.prisma = new exports.Prisma();
