63f7437c64fb45bf6e416f73089ab5ca4f0ca3d2
Thanh Dang <dang.np.thanh@gmail.com>
2019-02-24 03:42:57 +0200

    Add fix for SubscriptionDetailScreen

--- front-end/screens/MainScreens/MySubscriptionScreens/AddSubscriptionScreens/SubscriptionDetailScreen.js
+++ front-end/screens/MainScreens/MySubscriptionScreens/AddSubscriptionScreens/SubscriptionDetailScreen.js
@@ -5,17 +5,23 @@ import {
   Image,
   TouchableHighlight,
   Text,
+  ScrollView,
+  Platform,
 } from 'react-native';
 import { Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 import Colors from '../../../../constants/Colors';
 import commonStyles from '../../../../constants/commonStyles';
+import Layout from '../../../../constants/Layout';
 
 const boxNameToImageMapper = {
   'vegan box': require('../../../../assets/images/vegan.png'),
   'mixed box': require('../../../../assets/images/mixed.png'),
   'meat box': require('../../../../assets/images/meat.png'),
 };
+
+const width = Layout.window.width,
+  CONTENT_SEPARATOR = ',';
 
 class SubscriptionDetailScreen extends React.Component {
   static navigationOptions = {
@@ -36,6 +42,13 @@ class SubscriptionDetailScreen extends React.Component {
     },
   };
 
+  mockSampleContent = `Ground beef 2x1lb,NY strip 2x10oz,Top sirloin 4x6oz,Whole pork tenderloin 1lb,Boneless skinless chicken breast 3x1lb,*Exact cuts are subject to change`;
+
+  splitSampleContent = sampleContent => {
+    let splitSampleContent = sampleContent.split(CONTENT_SEPARATOR);
+    return splitSampleContent.map( (text, index) => <Text key={index} style={styles.descriptionContent}>{text}</Text>);
+  };
+
   render() {
     const { selectedSubscription } = this.props;
 
@@ -45,7 +58,7 @@ class SubscriptionDetailScreen extends React.Component {
     }
 
     return (
-      <View>
+      <ScrollView style={styles.mainContainer}>
         <View style={styles.topContainer}>
           <Image
             style={styles.image}
@@ -53,196 +66,187 @@ class SubscriptionDetailScreen extends React.Component {
               boxNameToImageMapper[selectedSubscription.title.toLowerCase()]
             }
           />
-          <View style={styles.priceTag} />
-          <Text style={styles.price}>
-            {selectedSubscription.totalPrice} {'\u20AC'}
+          <View style={styles.priceTag}>
+            <Text style={styles.priceText}>
+              {selectedSubscription.totalPrice} {'\u20AC'}
+            </Text>
+          </View>
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>{selectedSubscription.title}</Text>
+            <View style={styles.textTag}>
+              <Text style={styles.textTagText}>{selectedSubscription.tag}</Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.valueContainer}>
+          <Text style={styles.valueText}>
+            {selectedSubscription.size}kg/box
           </Text>
-        </View>
-        <View>
-          <View style={styles.titleContainer} />
-          <Text style={styles.title}>{selectedSubscription.title}</Text>
-          <View style={styles.tagBox} />
-          <Text style={styles.tag}>{selectedSubscription.tag}</Text>
-        </View>
-        <View>
-          <Text style={styles.size}>{selectedSubscription.size}kg/box</Text>
-          <View style={styles.dot} />
-          <Text style={styles.divprice}>
+          <View style={styles.circle} />
+          <Text style={styles.valueText}>
             {selectedSubscription.mealPrice}
             {'\u20AC'}/meal
           </Text>
         </View>
-        <View>
-          <Text style={styles.about}>ABOUT</Text>
-          <Text style={styles.description}>
-            {selectedSubscription.longDescription}
-          </Text>
+        <View style={{ paddingHorizontal: 40 }}>
+          <View>
+            <Text style={styles.descriptionTitle}>ABOUT</Text>
+            <Text style={[styles.descriptionContent, {marginTop: 10}]}>
+              {selectedSubscription.longDescription}
+            </Text>
+          </View>
+          <View>
+            <Text style={styles.descriptionTitle}>SAMPLE CONTENT</Text>
+            <View style={{marginTop: 10}}>
+              {this.splitSampleContent(this.mockSampleContent)}
+            </View>
+          </View>
         </View>
-        <View>
-          <Text style={styles.sampleTitle}>SAMPLE CONTENT</Text>
-          <Text style={styles.content}>
-            Ground beef - 2x1lb NY strip - 2x10oz Top sirloin - 4x6oz Whole pork
-            tenderloin - 1lb Boneless skinless chicken breast - 3x1lb *Exact
-            cuts are subject to change
-          </Text>
-        </View>
-      </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  about: {
+  mainContainer: {
+    flex: 1,
+  },
+  topContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: width,
+    height: 262,
+    backgroundColor: Colors.macaroniCheese,
+    paddingTop: 24,
+  },
+  image: {
+    width: 167,
+    height: 178,
+    marginTop: 44,
+  },
+  priceTag: Platform.select({
+    ios: {
+      position: 'absolute',
+      width: 70,
+      height: 70,
+      left: 257,
+      top: 64,
+      borderRadius: 38,
+      backgroundColor: Colors.mediumCarmine,
+      shadowColor: Colors.darkGrey,
+      shadowRadius: 4,
+      shadowOpacity: 0.25,
+      shadowOffset: { width: 0, height: 4 },
+    },
+    android: {
+      position: 'absolute',
+      width: 75,
+      height: 75,
+      left: 257,
+      top: 64,
+      borderRadius: 38,
+      backgroundColor: Colors.mediumCarmine,
+      elevation: 4,
+    },
+  }),
+  priceText: Platform.select({
+    ios: {
+      paddingTop: 22,
+      paddingLeft: 10,
+      ...commonStyles.fontRalewayBold,
+      fontSize: 20,
+      color: '#FFFFFF',
+    },
+    android: {
+      paddingTop: 22,
+      paddingLeft: 10,
+      ...commonStyles.fontRalewayBold,
+      fontSize: 22,
+      color: '#FFFFFF',
+    },
+  }),
+  titleContainer: {
+    width: '80%',
+    height: 124,
+
+    backgroundColor: Colors.white,
+    borderRadius: 12,
+
+    shadowColor: Colors.darkGrey,
+    shadowRadius: 4,
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 4 },
+
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 24,
+
     position: 'absolute',
-    width: 56,
-    height: 19,
-    left: 41,
-    top: 406,
+    top: 200,
+  },
+  title: {
+    ...commonStyles.fontRalewayBold,
+    color: Colors.black,
+    fontSize: 24,
+    textAlign: 'center',
+  },
+  textTag: {
+    paddingHorizontal: 24,
+    paddingVertical: 4,
+    height: 27,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.mediumCarmine,
+    shadowColor: Colors.darkGrey,
+    shadowRadius: 4,
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 4 },
+    borderRadius: 13.5,
+    marginTop: 20,
+  },
+  textTagText: {
     ...commonStyles.fontRalewayBold,
     fontSize: 16,
     textAlign: 'center',
-    color: Colors.macaroniCheese,
+    color: '#FFFFFF',
   },
-  content: {
-    position: 'absolute',
-    width: 300,
-    height: 112,
-    left: 41,
-    top: 537,
-    ...commonStyles.fontRalewayRegular,
-    fontSize: 14,
-    color: Colors.black,
-  },
-  description: {
-    position: 'absolute',
-    width: 300,
-    height: 77,
-    left: 41,
-    top: 433,
-    ...commonStyles.fontRalewayRegular,
-    fontSize: 14,
-    color: Colors.black,
-  },
-  divprice: {
-    position: 'absolute',
-    width: 74,
-    height: 17,
-    left: 217,
-    top: 355,
+  size: {
     ...commonStyles.fontRalewayBold,
+    color: Colors.darkGrey,
     fontSize: 14,
     textAlign: 'center',
-    color: Colors.darkGrey,
   },
-  dot: {
-    position: 'absolute',
+  valueContainer: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+    paddingTop: 3,
+    marginTop: 88
+  },
+  circle: {
+    marginTop: 6,
+    marginLeft: 12,
+    marginRight: 12,
     width: 8,
     height: 8,
-    left: 197,
-    top: 360,
     borderRadius: 4,
     backgroundColor: Colors.mediumCarmine,
   },
-  image: {
-    position: 'absolute',
-    width: 167,
-    height: 178,
-    left: 124,
-    top: 66,
-  },
-  priceTag: {
-    position: 'absolute',
-    width: 75,
-    height: 75,
-    left: 261,
-    top: 56,
-    borderRadius: 37.5,
-    backgroundColor: Colors.mediumCarmine,
-    shadowRadius: 4,
-    shadowOffset: { height: 0, width: 4 },
-    shadowColor: 'rgba(91, 91, 91, 0.25)',
-  },
-  price: {
-    position: 'absolute',
-    width: 58,
-    height: 26,
-    left: 270,
-    top: 78,
-    ...commonStyles.fontRalewayBold,
-    fontSize: 22,
-    color: Colors.white,
-  },
-  sampleTitle: {
-    position: 'absolute',
-    width: 144,
-    height: 19,
-    left: 41,
-    top: 510,
-    ...commonStyles.fontRalewayBold,
-    fontSize: 16,
-    color: Colors.macaroniCheese,
-  },
-  size: {
-    position: 'absolute',
-    width: 62,
-    height: 17,
-    left: 123,
-    top: 355,
+  valueText: {
     ...commonStyles.fontRalewayBold,
     fontSize: 14,
     textAlign: 'center',
     color: Colors.darkGrey,
   },
-  tag: {
-    position: 'absolute',
-    width: 49,
-    height: 19,
-    left: 183,
-    top: 287,
+  descriptionTitle: {
     ...commonStyles.fontRalewayBold,
+    color: Colors.macaroniCheese,
     fontSize: 16,
-    textAlign: 'center',
-    color: Colors.white,
+    marginTop: 34,
   },
-  tagBox: {
-    position: 'absolute',
-    width: 127,
-    height: 27,
-    left: 144,
-    top: 283,
-    borderRadius: 13.5,
-    backgroundColor: Colors.mediumCarmine,
-  },
-  title: {
-    position: 'absolute',
-    width: 268,
-    height: 29,
-    left: 73,
-    top: 234,
-    ...commonStyles.fontRalewayBold,
-    fontSize: 24,
-    textAlign: 'center',
+  descriptionContent: {
+    ...commonStyles.fontRalewayRegular,
     color: Colors.black,
-  },
-  titleContainer: {
-    position: 'absolute',
-    width: 335,
-    height: 124,
-    left: 40,
-    top: 210,
-    backgroundColor: Colors.white,
-    shadowRadius: 4,
-    shadowOffset: { height: 0, width: 4 },
-    shadowColor: 'rgba(91, 91, 91, 0.25)',
-    borderRadius: 12,
-  },
-  topContainer: {
-    position: 'absolute',
-    width: 414,
-    height: 262,
-    left: 0,
-    top: 0,
-    backgroundColor: Colors.macaroniCheese,
+    fontSize: 14,
   },
 });
 
@@ -250,7 +254,4 @@ const mapStateToProps = state => ({
   selectedSubscription: state.checkout.selectedSubscription,
 });
 
-export default connect(
-  mapStateToProps,
-  null
-)(SubscriptionDetailScreen);
+export default connect(mapStateToProps, null)(SubscriptionDetailScreen);
